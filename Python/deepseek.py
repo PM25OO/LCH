3cc2c2d5398c54d4ea8039df0ae2cd3bfa1ba0b8
PM25OO <3210636476@qq.com>
2025-04-07 14:49:03 +0800

    update

--- Python/deepseek.py
+++ Python/deepseek.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-API_KEY = os.getenv('API_KEY')
+API_KEY = os.getenv("Deepseek_API_KEY")
 
 client = OpenAI(api_key=API_KEY, base_url="https://api.deepseek.com")
 messages = [{"role": "system", "content": "你是一个乐于助人的助手，用中文回答问题。"}]
@@ -16,18 +16,16 @@ while True:
     messages.append({"role": "user", "content": user_input})
 
     response = client.chat.completions.create(
-        model="deepseek-reasoner",
-        messages=messages,
-        stream=True
+        model="deepseek-reasoner", messages=messages, stream=True
     )
-    
+
     reasoning_content = ""
     content = ""
-    should_print1, should_print2 = True, True 
-    
+    should_print1, should_print2 = True, True
+
     print(f"\n思考中...\n")
     for chunk in response:
-        
+
         if chunk.choices[0].delta.reasoning_content:
             print(chunk.choices[0].delta.reasoning_content, end="")
             reasoning_content += chunk.choices[0].delta.reasoning_content
@@ -37,7 +35,6 @@ while True:
                 should_print2 = False
             print(chunk.choices[0].delta.content, end="")
             content += chunk.choices[0].delta.content
-        
 
     print(f"\n")
 
